01e46c640031d1168e0a12d41f7dae9ef86c27fb
Marija Robinson <marija@springmail.com>
2016-10-31 13:35:36 -0500

    Continue working on map function

--- main.py
+++ main.py
@@ -288,14 +288,15 @@ def showStates():
   siteList = []
   for a in allSites:
     siteList.append(a.state_id)
+
   try:
     currentUserID = login_session['user_id']
     if currentUserID:
       return render_template('states.html', states = states, sites = siteList, currentUserID = currentUserID, allSites = allSites)
     else:
-      return render_template('publicstates.html', states = states, sites = siteList, allSites = allSites)
+      return render_template('publicstates.html', states = states, sites = siteList, allSites = allSites, jsSites = [s.serialize for s in allSites], jsStates = [s.serialize for s in states])
   except:
-    return render_template('publicstates.html', states = states, sites = siteList, allSites = allSites)
+    return render_template('publicstates.html', states = states, sites = siteList, allSites = allSites, jsSites = [s.serialize for s in allSites], jsStates = [s.serialize for s in states])
 
 # Need to add functionality that requires user to add a site
 # when adding a state, and also check to make sure they only add
